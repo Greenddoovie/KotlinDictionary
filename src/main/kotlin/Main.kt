acaf700a3cbcca4782863f1edb960f11e8ac5661
K057_최연두 <75981415+Greenddoovie@users.noreply.github.com>
2021-10-26 00:01:37 +0900

    운영 방식 변경

--- src/main/kotlin/Main.kt
+++
@@ -1,6 +0,0 @@
-fun main(args: Array<String>) {
-    println("Hello Kotlin Beginner!")
-
-    // Try adding program arguments at Run/Debug configuration
-    println("Spread your knowledge to this repository about Kotlin Usage!!")
-}
